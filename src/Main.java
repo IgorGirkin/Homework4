353206aaaac3e88825f6386ccaa83a2b35db600f
grisha <norsulfasol@gmail.com>
2022-10-01 21:53:48 +0300

    ДЗ 4 Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int years = 18;
-        if (years>=18) {
-        System.out.println("Поздравляем пользователя с совершеннолетием!");
+        int childAge=7;
+        int student=18;
+        int generalWorker=24;
+        if (childAge>=7) {
+        System.out.println("Сегодня 1 сентября, прощай интернет, пора идти в школу!");
         }
-        if (years<18) {System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного " +
-                "подождать.");
+        if (student>=18) {System.out.println("Возраст совершеннолетия, привет общага, " +
+                "можешь отправляться в университет!");
+        }
+        if (generalWorker>=24) {
+            System.out.println("А вот и пришла пора отправляться на поиски приключений, " +
+                    "там ты найдешь золотые монеты на поле чудес!... пора искать первую работу.");
         }
     }
 }
