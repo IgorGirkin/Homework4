11de0585318a7ccbb1b48380e091bb4427a8961e
grisha <norsulfasol@gmail.com>
2022-10-01 21:27:05 +0300

    ДЗ4 Задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int years = 18;
+        if (years>=18) {
+        System.out.println("Поздравляем пользователя с совершеннолетием!");
+        }
+        if (years<18) {System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного " +
+                "подождать.");
+        }
+    }
+}
