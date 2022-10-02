ea97486668d4e446b9c8f1c4c267358156242b5f
grisha <norsulfasol@gmail.com>
2022-10-02 20:18:03 +0300

    ДЗ 4 Часть 3 Задание 3 Условный оператор.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int ageСhild = 14;
+        int ageСhild = 15;
         boolean togetherWithAdults=ageСhild >= 5 && ageСhild <= 14;
         if (togetherWithAdults) {
             System.out.println("Ребенок может кататься только в сопровождении взрослого!");
         } else if (ageСhild<5) {
-            System.out.println("Ребенок НЕ может кататься на аттракционе!"); }
-
-        if (ageСhild>14) {
+            System.out.println("Ребенок НЕ может кататься на аттракционе!");
+        } else if (ageСhild>14) {
             System.out.println("Ребенок может кататься без сопровождения взрослого!");
         }
 
